490d0cfa46608ab19678619cdbb5a9750283ed33
catarina cordeiro <catie.cord@gmail.com>
2024-02-07 18:01:53 +0000

    database model and cards

--- ccm_app/views.py
+++ ccm_app/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
+
 
 def home(request):
-
+    records = Record.objects.all()
+    # Check to see if loggin in
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +20,8 @@ def home(request):
             messages.success(request, ('Error logging in - please try again...'))
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
+
 
 def logout_user(request):
     logout(request)
@@ -39,5 +43,26 @@ def register_user(request):
             return redirect('home')
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form':form})
-    return render(request, 'register.html', {'form':form})
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
+
+
+def payment_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up the payment record
+        pay_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'record': pay_record})
+    else:
+        messages.success(request, 'You must be logged in to view records!')
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, 'Record has been deleted!')
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to delete records!')
+        return redirect('home')
