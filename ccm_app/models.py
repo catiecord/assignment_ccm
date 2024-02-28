7a9bf037cb4d890d689b2fd3aa443e66759181c2
catarina cordeiro <catie.cord@gmail.com>
2024-02-28 18:03:55 +0000

    implement audit and user management logic

--- ccm_app/models.py
+++ ccm_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Record(models.Model):
@@ -10,6 +11,26 @@ class Record(models.Model):
     contact_date = models.DateTimeField()
     contact_status = models.CharField(max_length=60)
     notes = models.TextField()
+
+class Audit(models.Model):
+    """
+    Represents an audit log entry to track user actions on knowledge base entries.
+
+    Attributes:
+    - user (ForeignKey): Reference to the user who performed the action.
+    - action_datetime (DateTimeField): The date and time when the action was performed. Automatically set to the current date and time when a new log entry is created.
+    - kb_entry (ForeignKey): Reference to the knowledge base entry that the action was performed on. Can be null if the action is not related to a specific entry.
+    - action_details (CharField): A brief description of the action performed. Maximum length of 255 characters.
+
+    The Audit model is designed to keep a log of various actions that users perform within the application,
+    thereby allowing for accountability and potential analysis of user activity.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    action_datetime = models.DateTimeField(auto_now_add=True)
+    database_entry = models.ForeignKey(Record, on_delete=models.SET_NULL, null=True)
+    action_details = models.CharField(max_length=255)
+    ip_address = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return (f"{self.payment_reference} {self.first_name} {self.last_name}")
--- ccm_app/views.py
+++ ccm_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
 from .forms import SignUpForm, AddRecordForm
-from .models import Record
+from .models import Record, Audit
 
 
 def home(request):
@@ -99,3 +99,102 @@ def update_record(request, pk):
     else:
         messages.success(request, 'You must be logged in to update records!')
         return redirect('home')
+
+def user_management(request):
+    if request.user.is_authenticated:
+        if request.user.is_staff:
+            User = get_user_model()
+            users = User.objects.all()
+            return render(request, 'user_management.html', {'users': users})
+        else:
+            messages.success(request, 'You must be an admin to view users!')
+            return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to view users!')
+        return redirect('home')
+def user_logs(request):
+    """
+    Displays a page with a list of all audit logs to the superuser.
+
+    - Accessible only to authenticated users due to the @login_required decorator.
+    - Checks if the user is a superuser:
+        - If not, the user is redirected to the home page with an error message.
+    - If the user is a superuser:
+        - Fetches all Audit objects from the database and orders them by the action_datetime field in descending order.
+        - Renders the 'knowledge/audit_logs.html' template with the context containing the fetched Audit objects.
+
+    Parameters:
+    - request (HttpRequest): The HTTP request object, which contains information about the current user.
+
+    Returns:
+    - HttpResponse: The rendered HTML page for superusers, or a redirect to the home page for non-superusers.
+    """
+    if not request.user.is_superuser:
+        messages.error(request, "You don't have permission to view this page.")
+        return redirect("home")
+
+    # Fetch all audit logs
+    logs = Audit.objects.all().order_by(
+        "-action_datetime"
+    )  # Most recent actions first
+
+    context = {"logs": logs}
+
+    return render(request, "user_logs.html", context)
+
+def user_active_status(request, user_id):
+    """
+    Toggles the active status of a user account.
+
+    - Accessible only to authenticated users due to the @login_required decorator.
+    - Checks if the user is a superuser:
+        - If not, the user is redirected to the home page with an error message.
+    - If the user is a superuser:
+        - Attempts to fetch the User object that corresponds to the provided user_id parameter.
+        - If the User object is not found, an error message is displayed, and the user is redirected to the user list page.
+        - If the User object is found:
+            - Checks if the user to be toggled is the same as the logged-in superuser:
+                - If yes, an error message is displayed, indicating that the superuser cannot deactivate their own account.
+                - If no, the active status (is_active attribute) of the User object is toggled (True becomes False, and vice versa).
+                - Saves the updated User object to the database.
+                - Displays a success message indicating whether the account has been activated or deactivated.
+
+    Parameters:
+    - request (HttpRequest): The HTTP request object, which contains information about the current user.
+    - user_id (int): The ID of the user whose active status is to be toggled.
+
+    Returns:
+    - HttpResponse: Redirects to the user list page with a success message for superusers, or to the home page with an error message for non-superusers.
+    """
+    User = get_user_model()
+    # Check if the user is a superuser
+    if not request.user.is_superuser:
+        messages.error(request, "You do not have permission to perform this action.")
+        return redirect("home")
+
+    # Get the user by ID
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        messages.error(request, "User not found.")
+        return redirect("user_management")
+
+    # Check if the user to toggle is the same as the logged-in superuser
+    if user == request.user:
+        messages.error(request, "You cannot deactivate your own account!")
+        return redirect("user_management")
+
+    # Toggle the user's active status
+    user.is_active = not user.is_active
+    user.save()
+
+    # Provide feedback to the superuser
+    if user.is_active:
+        messages.success(
+            request, f"{user.username}'s account has been activated."
+        )
+    else:
+        messages.success(
+            request, f"{user.username}'s account has been deactivated."
+        )
+    return redirect("user_management")
